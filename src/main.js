570d6c39089ac18ad618463cbf405cf877b94793
xzliang <yeyuyixiao@163.com>
2018-09-03 07:45:39 +0800

    配置首页webpack字体图标问题卡了很久

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+// 导入全局样式和导入bootstrap样式
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
