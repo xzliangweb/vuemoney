e9530275d150dcdc3a41363f9cfe0241934663ac
xzliang <yeyuyixiao@163.com>
2018-09-04 23:29:32 +0800

    linkActiveClass 路由中包含a标签的地址

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,6 +4,7 @@ import Vue from "vue";
 import heroesList from "../views/heroes/List.vue";
 import equipsList from "../views/equips/List.vue";
 import weaponsList from "../views/weapons/List.vue";
+import HeroAdd from "../views/heroes/Add.vue";
 
 // Vue.use()注册VueRouter 插件
 Vue.use(VueRouter);
@@ -13,12 +14,15 @@ const router = new VueRouter({
 
     // 设置精确的类样式  
     // url中的路由地址，包含a标签的路由地址，就高亮显示
-    linkExactActiveClass: 'active',
+    // linkExactActiveClass: 'active', 精确的查找
+    // url中的路由地址包含a标签中的路由地址
+    linkActiveClass:'active',
     routes:[ 
         {path:'/',redirect:'/heroes'},   
         {path:'/heroes',component:heroesList},
         {path:'/equips',component:equipsList},
-        {path:'/weapons',component:weaponsList}
+        {path:'/weapons',component:weaponsList},
+        {path:'/heroadd',component:HeroAdd}
     ]
 });
 // 到出对象
---
+++ src/views/heroes/Add.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="sub-header">添加英雄</h2>
+    <form>
+      <div class="form-group">
+        <label for="txtName">英雄名称</label>
+        <input v-model="formData.name" type="text" class="form-control" id="txtName" placeholder="英雄名称">
+      </div>
+      <div class="form-group">
+        <label for="txtgender">英雄性别</label>
+        <input v-model="formData.gender" type="text" class="form-control" id="txtgender" placeholder="性别">
+      </div>
+      <button @click.prevent="handleAdd" type="submit" class="btn btn-success">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
